d802a31ea3ff22e23cb1f77a805538967cd8fca2
Juan Castro <juancastro036@users.nooreply.github.com>
2016-11-10 14:00:04 -0800

    started
    made gameboard

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,13 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var currentSquare = "topleftsquare"
+function game(){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", "50")
+  circle.setAttribute("cy", "50")
+  circle.setAttribute("r", "50")
+  circle.setAttribute("fill", "red")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
